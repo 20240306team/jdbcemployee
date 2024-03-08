58f74088a959226d36b81716129d86fbbcf9d9d8
rjadmsehf <jjg524@naver.com>
2024-03-08 16:00:54 +0900

    select 완성

--- src/main/java/com/common/employeeDAO.java
+++ src/main/java/com/common/employeeDAO.java
@@ -1,8 +1,49 @@
 package com.common;
 
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+import static com.common.JDBCTemplate.close;
+
 public class employeeDAO {
 
-    public static void main(String[] args) {
-        
+    private Properties prop = new Properties();
+
+    public employeeDAO(){
+        try {
+            prop.loadFromXML(new FileInputStream("src/main/java/com/mapper/employee-query.xml"));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+    public void countEmp (Connection con ){
+
+        Statement stmt = null;
+        ResultSet rset = null;
+
+        String query = prop.getProperty("select");
+
+        try {
+            stmt = con.createStatement();
+            rset = stmt.executeQuery(query);
+            while (rset.next()){
+                System.out.println(rset.getString("부서명") +" , " + rset.getString("부서인원") );
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            close(stmt);
+            close(rset);
+        }
+
     }
 }
+
+
--- src/main/java/com/run/Application.java
+++ src/main/java/com/run/Application.java
@@ -1,8 +1,28 @@
 package com.run;
 
-public class Application {
+import com.common.employeeDAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static com.common.JDBCTemplate.close;
+import static com.common.JDBCTemplate.getConnection;
+
+public class Application {
     public static void main(String[] args) {
+
+    Connection con = getConnection();
+
+    employeeDAO select = new employeeDAO();
+
+    select.countEmp(con);
 
     }
 }
+
+
+
+
+
